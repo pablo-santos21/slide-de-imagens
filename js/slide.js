31cd2a68a5d70ad4a2d67afbd3ea6c584548d495
Pablo Santos <binho.bgu@gmail.com>
2022-08-09 23:02:31 -0300

    Realizando calculos do slide

--- js/slide.js
+++ js/slide.js
@@ -2,20 +2,37 @@ export default class slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      moviment: 0,
+    };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.moviment = (this.dist.startX - clientX) * 1;
+    return this.dist.finalPosition - this.dist.moviment;
   }
 
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
   onMove(event) {
-    console.log("moveu");
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
-    console.log("acabou");
     this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlidEvents() {
